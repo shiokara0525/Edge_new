39bf2eb05c82b29cf88e82de19882d6cee8e2e4c
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-01 17:42:32 +0900

    Neopixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
 #include <Arduino.h>
+#include<Adafruit_NeoPixel.h>
 #include<Cam.h>
 
+#define DELAYVAL 500
+#define PIN        30 
+#define NUMPIXELS 16
+
+
+Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial4.begin(9600);
   Serial.begin(9600);
+  pixels.begin();
 }
 
 void loop() {
-  if(0 < Serial4.available()){
-    int A = Serial4.read();
-    Serial.println(A);
+  pixels.clear(); // Set all pixel colors to 'off'
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one.
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+
+    delay(DELAYVAL); // Pause before next pass through loop
   }
-  else{
-    Serial.println("sawA");
-  }
-  delay(100);
 }
