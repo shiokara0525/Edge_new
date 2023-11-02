8130d886b920d1e9bb66ce0c8ae69262e0d0c1ce
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-02 17:35:56 +0900

    きっくするよ（できない）

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,31 @@
 #include<Adafruit_NeoPixel.h>
 #include<Cam.h>
 
+const int C = 32;
+const int K = 31;
+const int LED = 13;
 #define DELAYVAL 500
 #define PIN        30 
 #define NUMPIXELS 16
 
-
 Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(C,OUTPUT);
+  pinMode(K,OUTPUT);
+  pinMode(LED,OUTPUT);
   pixels.begin();
+  pixels.clear();
+  digitalWrite(C,HIGH);
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
-
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL); // Pause before next pass through loop
-  }
+  digitalWrite(K,LOW);
+  digitalWrite(LED,LOW);
+  delay(5000);
+  digitalWrite(K,HIGH);
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(K,LOW);
+  digitalWrite(LED,LOW);
 }
