db5709773219e6bf47b1797a78436f0b17e7991f
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-11 10:15:22 +0900

    ライン受け取るよ

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,39 @@ const int LED = 13;
 
 Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
 
+int x = 0;
+int y = 0;
+int num = 0;
+
 void setup() {
-  pinMode(K,OUTPUT);
-  pinMode(C,OUTPUT);
-  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+  Serial6.begin(9600);
   pixels.begin();
-  pixels.clear();
-  digitalWrite(C,HIGH);
 }
 
 void loop() {
-  digitalWrite(K,LOW);
-  digitalWrite(LED,LOW);
-  delay(5000);
-  digitalWrite(K,HIGH);
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(K,LOW);
-  digitalWrite(LED,LOW);
+}
+
+
+void serialEvent6(){
+  uint8_t read[7];
+  if(7 <= Serial6.available()){
+    for(int i = 0; i < 5; i++){
+      read[i] = Serial6.read();
+    }
+    while(Serial6.available()){
+      Serial6.read();
+    }
+  }
+
+  if(read[0] == 38 && read[4] == 37){
+    x = read[1];
+    y = read[2];
+    num = read[3];
+  }
+  Serial.print(x);
+  Serial.print(" ");
+  Serial.print(y);
+  Serial.print(" ");
+  Serial.println(num);
 }
