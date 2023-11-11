e3236fe1e48af3bde9c9700961f407ad66b9a12d
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-11 16:26:17 +0900

    ラグはあるけど追えるよ

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,65 @@
 #include <Arduino.h>
 
-int sawa[2] = {34,35};
 int LED = 13;
+int X;
+int Y;
 
 void setup() {
-  Serial.begin(9600);
-  Serial8.begin(9600);
-  for(int i = 0; i < 2; i++){
-    pinMode(sawa[i],OUTPUT);
-    digitalWrite(sawa[i],HIGH);
-  }
+  Serial.begin(57600);
+  Serial8.begin(57600);
 }
 
 void loop() {
-  if(Serial8.available()){
-    Serial.println("SAWA");
-  }
 }
 
 
 void serialEvent8(){
-  digitalWrite(LED,HIGH);
-  delay(500);
-  digitalWrite(LED,LOW);
-  delay(500);
+  int n;
+  int x,y;
+  word revBuf_word[6];
+  byte revBuf_byte[6];
+  //受信データ数が、一定時間同じであれば、受信完了としてデータ読み出しを開始処理を開始する。
+  //受信データあり ※6バイト以上になるまでまつ
+  if(Serial8.available()>= 6){
+    //---------------------------
+    //受信データをバッファに格納
+    //---------------------------
+    n = 0;
+    while(Serial8.available()>0 ){ //受信データがなくなるまで読み続ける
+      //6バイト目まではデータを格納、それ以上は不要なデータであるため捨てる。
+      if(n < 6){
+        revBuf_byte[n] = Serial8.read();   //revBuf_byte[n] = Serial2.read();
+      }
+      else{
+        Serial8.read(); //Serial2.read();  //読みだすのみで格納しない。
+      }
+      n++;
+    }
+    //---------------------------
+    //データの中身を確認
+    //---------------------------
+    //データの先頭、終了コードあることを確認
+    if((revBuf_byte[0] == 0xFF ) && ( revBuf_byte[5] == 0xAA )){
+    //いったんWORD型（16bitデータ）としてから、int16_tとする。
+      revBuf_word[0] = (uint16_t(revBuf_byte[1])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
+      revBuf_word[1] = uint16_t(revBuf_byte[2]);//下位8ビットをbyteから、Wordに型変換して格納      
+      x = int16_t(revBuf_word[0]|revBuf_word[1]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
+      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
+      revBuf_word[2] = (uint16_t(revBuf_byte[3])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
+      revBuf_word[3] = uint16_t(revBuf_byte[4]);//下位8ビットをbyteから、Wordに型変換して格納      
+      y = int16_t(revBuf_word[2]|revBuf_word[3]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
+      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
+      
+      X = x;
+      Y = y;
+    }
+    else{
+      // printf("ERR_REV");
+    }
+  }
+  Serial.print(X);
+  Serial.print(" ");
+  Serial.print(Y);
+  Serial.print(" ");
+  Serial.println(degrees(atan2(Y,X)));
 }
