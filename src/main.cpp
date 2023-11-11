d54d4793f08436b3145f7306f0eeb6cbfc93a9dc
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-11 15:43:35 +0900

    ボールの確認するよ～

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,27 @@
 #include <Arduino.h>
-#include<Adafruit_NeoPixel.h>
-#include<Cam.h>
 
-#define DELAYVAL 500
-#define PIN        30 
-#define NUMPIXELS 16
-
-
-Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
+int sawa[2] = {34,35};
+int LED = 13;
 
 void setup() {
   Serial.begin(9600);
-  pixels.begin();
+  Serial8.begin(9600);
+  for(int i = 0; i < 2; i++){
+    pinMode(sawa[i],OUTPUT);
+    digitalWrite(sawa[i],HIGH);
+  }
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
-
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL); // Pause before next pass through loop
+  if(Serial8.available()){
+    Serial.println("SAWA");
   }
+}
+
+
+void serialEvent8(){
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
+  delay(500);
 }
