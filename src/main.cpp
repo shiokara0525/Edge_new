c6d847d34b8db9f0ac5efd45e79b295fc11607f5
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-11-11 15:24:56 +0900

    うまくいってないよ

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,25 @@
 #include <Arduino.h>
-#include<Adafruit_NeoPixel.h>
-#include<Cam.h>
 
-#define DELAYVAL 500
-#define PIN        30 
-#define NUMPIXELS 16
-
-
-int A = 1;
-int val = 100;
 int PWM_p[5][2] = {
   {7,6},{2,3},{4,5},{8,9},{0,1}
 };
 int LED = 13;
-int ang;
-void motor(float ang);
 
 void setup() {
   Serial.begin(9600);
-  Serial8.begin(57600);
-  for(int i = 0; i < 5; i++){
-    pinMode(PWM_p[i][0],OUTPUT);
-    pinMode(PWM_p[i][1],OUTPUT);
-  }
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,HIGH);
-  delay(400);
-  digitalWrite(LED,LOW);
-  delay(400);
-  digitalWrite(LED,HIGH);
+  Serial8.begin(9600);
 }
 
 void loop() {
-  //motor(90);
-}
-
-
-
-void motor(float ang){
-  double mSin[4] = {1,1,-1,-1};  //行列式のsinの値
-  double mCos[4] = {1,-1,-1,1};  //行列式のcosの値
-  float X = cos(radians(ang));
-  float Y = sin(radians(ang));
-  float A = 0;
-  float B = 0;
-  float Mval[4];
-  double g = 0;                //モーターの最終的に出る最終的な値の比の基準になる値
-  for(int i = 0; i < 4; i++){
-    A = -mSin[i] * X;
-    B = mCos[i] * Y;
-    Mval[i] = A + B; //モーターの回転速度を計算(行列式で管理)
-    
-    if(abs(Mval[i]) > g){  //絶対値が一番高い値だったら
-      g = abs(Mval[i]);    //一番大きい値を代入
-    }
-  }
-
-  for(int i = 0; i < 4; i++){
-    Mval[i] = Mval[i] / g * val;  //モーターの値を計算(進みたいベクトルの値と姿勢制御の値を合わせる)
-    if(Mval[i] < 0){
-      analogWrite(PWM_p[i][0],abs(Mval[i]));
-      analogWrite(PWM_p[i][1],0);
-    }
-    else if(0 < Mval[i]){
-      analogWrite(PWM_p[i][0],0);
-      analogWrite(PWM_p[i][1],abs(Mval[i]));
-    }
-
+  if(Serial8.available()){
+    Serial.println("SAWA");
   }
 }
 
 
 void serialEvent8(){
-  int n;
-  int x,y;
-  word revBuf_word[6];
-  byte revBuf_byte[6];
-  //受信データ数が、一定時間同じであれば、受信完了としてデータ読み出しを開始処理を開始する。
-  //受信データあり ※6バイト以上になるまでまつ
-  if(Serial8.available()>= 6){
-    //---------------------------
-    //受信データをバッファに格納
-    //---------------------------
-    n = 0;
-    while(Serial8.available()>0 ){ //受信データがなくなるまで読み続ける
-      //6バイト目まではデータを格納、それ以上は不要なデータであるため捨てる。
-      if(n < 6){
-        revBuf_byte[n] = Serial8.read();   //revBuf_byte[n] = Serial2.read();
-      }
-      else{
-        Serial8.read(); //Serial2.read();  //読みだすのみで格納しない。
-      }
-      n++;
-    }
-    //---------------------------
-    //データの中身を確認
-    //---------------------------
-    //データの先頭、終了コードあることを確認
-    if((revBuf_byte[0] == 0xFF ) && ( revBuf_byte[5] == 0xAA )){
-    //いったんWORD型（16bitデータ）としてから、int16_tとする。
-      revBuf_word[0] = (uint16_t(revBuf_byte[1])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
-      revBuf_word[1] = uint16_t(revBuf_byte[2]);//下位8ビットをbyteから、Wordに型変換して格納      
-      x = int16_t(revBuf_word[0]|revBuf_word[1]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
-      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
-      revBuf_word[2] = (uint16_t(revBuf_byte[3])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
-      revBuf_word[3] = uint16_t(revBuf_byte[4]);//下位8ビットをbyteから、Wordに型変換して格納      
-      y = int16_t(revBuf_word[2]|revBuf_word[3]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
-      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
-      
-      ang = degrees(atan2(y,x));
-    }
-    else{
-      // printf("ERR_REV");
-    }
-  }
-  Serial.println(ang);
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
+  delay(500);
 }
