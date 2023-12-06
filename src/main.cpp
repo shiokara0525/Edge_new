6ee04c1c61016dee1851120474a2672e5d83ea9d
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-12-07 08:33:36 +0900

    カメラ後ろと前どっちも見れるよ

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include<Cam.h>
 #include<ac.h>
 
-Cam cam(3);
+Cam cam_front(4);
+Cam cam_back(3);
 
 void setup() {
   Serial.begin(9600);
@@ -13,7 +14,7 @@ void setup() {
 
 void loop() {
   delay(2);
-  cam.print();
+  cam_front.print();
 }
 
 
@@ -34,12 +35,42 @@ void serialEvent3(){
 
   if(reBuf[0] == 38 && reBuf[3] == 37){
     if(reBuf[2] == 0){
-      cam.on = 0;
+      cam_back.on = 0;
     }
     else{
-      cam.on = 1;
-      cam.Size = reBuf[2];
-      cam.ang = reBuf[1] - 127;
+      cam_back.on = 1;
+      cam_back.Size = reBuf[2];
+      cam_back.ang = reBuf[1] - 127;
+    }
+  }
+  Serial.println("sawa");
+}
+
+
+
+void serialEvent4(){
+  uint8_t reBuf[4];
+  if(Serial4.available() < 4){
+    return;
+  }
+
+  for(int i = 0; i < 4; i++){
+    reBuf[i] = Serial4.read();
+    // Serial.print(reBuf[i]);
+    // Serial.print(" ");
+  }
+  while(Serial4.available()){
+    Serial4.read();
+  }
+
+  if(reBuf[0] == 38 && reBuf[3] == 37){
+    if(reBuf[2] == 0){
+      cam_front.on = 0;
+    }
+    else{
+      cam_front.on = 1;
+      cam_front.Size = reBuf[2];
+      cam_front.ang = reBuf[1] - 127;
     }
   }
   Serial.println("sawa");
