8f6e0c6c1c8ff6a93c67bc19692be696f38202fa
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-12-06 18:24:49 +0900

    一応見れたよ

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,24 @@ void setup() {
 }
 
 void loop() {
-  delay(100);
+  delay(2);
   cam.print();
 }
 
 
 void serialEvent3(){
   uint8_t reBuf[4];
-  if(4 < Serial3.available()){
-    for(int i = 0; i < 4; i++){
-      reBuf[i] = Serial3.read();
-      // Serial.print(reBuf[i]);
-      // Serial.print(" ");
-    }
+  if(Serial3.available() < 4){
+    return;
   }
-  else{
-    reBuf[0] = Serial3.read();
-    // Serial.print(reBuf[0]);
+
+  for(int i = 0; i < 4; i++){
+    reBuf[i] = Serial3.read();
+    // Serial.print(reBuf[i]);
     // Serial.print(" ");
+  }
+  while(Serial3.available()){
+    Serial3.read();
   }
 
   if(reBuf[0] == 38 && reBuf[3] == 37){
@@ -39,7 +39,7 @@ void serialEvent3(){
     else{
       cam.on = 1;
       cam.Size = reBuf[2];
-      cam.ang = reBuf[1] - 30;
+      cam.ang = reBuf[1] - 127;
     }
   }
   Serial.println("sawa");
