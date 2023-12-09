6b5af7a297872a7ca9d61f199e32b385b79f819b
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-12-09 10:33:02 +0900

    キッカーつくかも!?!?!?

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,27 @@
 #include<Adafruit_NeoPixel.h>
 #include<Cam.h>
 
-const int C = 35;
-const int K = 33;
+const int C = 31;
+const int K = 32;
 const int LED = 13;
-#define DELAYVAL 500
-#define PIN        30 
-#define NUMPIXELS 16
 
-Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pinMode(K,OUTPUT);
   pinMode(C,OUTPUT);
   pinMode(LED,OUTPUT);
-  pixels.begin();
-  pixels.clear();
   digitalWrite(C,HIGH);
+  digitalWrite(K,HIGH);
+  digitalWrite(LED,HIGH);
 }
 
 void loop() {
-  digitalWrite(K,LOW);
-  digitalWrite(LED,LOW);
-  delay(5000);
-  digitalWrite(K,HIGH);
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(K,LOW);
-  digitalWrite(LED,LOW);
+  // digitalWrite(K,LOW);
+  // digitalWrite(LED,LOW);
+  // delay(5000);
+  // digitalWrite(K,HIGH);
+  // digitalWrite(LED,HIGH);
+  // delay(1000);
+  // digitalWrite(K,LOW);
+  // digitalWrite(LED,LOW);
 }
