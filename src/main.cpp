ea4a89ac112d83d40197aa6d765ae917a091d164
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-12-11 09:11:35 +0900

    PWM周波数はレジスタのポートごとに変わるっぽい

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
 
 #define Pin 33
+#define Pin_1 36
+#define Pin_2 37
+#define Pin_3 10
+#define Pin_4 39
 #define LED 13
 
 void setup() {
   Serial.begin(9600);
   pinMode(Pin,OUTPUT);
+  pinMode(Pin_1,OUTPUT);
+  pinMode(Pin_2,OUTPUT);
+  pinMode(Pin_3,OUTPUT);
+  pinMode(Pin_4,OUTPUT);
   pinMode(LED,OUTPUT);
   analogWriteFrequency(Pin,60000);
   analogWrite(Pin,100);
+  analogWrite(Pin_1,100);
+  analogWrite(Pin_2,100);
+  analogWrite(Pin_3,100);
+  analogWrite(Pin_4,100);
   digitalWrite(LED,HIGH);
 }
 
