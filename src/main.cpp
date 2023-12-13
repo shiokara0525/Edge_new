95acd9ad0298615e3fb7cbeea8ca7cd1ae2f4b92
Yuta Nakagawa <yuta0525ny@gmail.com>
2023-12-13 18:18:32 +0900

    なんかラグすごい

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,92 @@
 #include <Arduino.h>
 #include<Adafruit_NeoPixel.h>
-#include<Cam.h>
+#include<ball.h>
 
 #define DELAYVAL 500
 #define PIN        30 
 #define NUMPIXELS 16
 
+int Neo[16] = {12,11,10,9,8,7,6,5,4,3,2,1,0,15,14,13};
+int Neo_p = 999;
 
 Adafruit_NeoPixel pixels(DELAYVAL, PIN, NEO_GRB + NEO_KHZ800);
+BALL ball;
+
 
 void setup() {
   Serial.begin(9600);
+  Serial6.begin(57600);
+  Serial8.begin(57600);
   pixels.begin();
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  ball.getBallposition();
+  pixels.clear();
+  for(int i = 0; i < 16; i++){
+    if(ball.ang < -180 + (i + 1) * 22.5){
+      pixels.setPixelColor(Neo[i], pixels.Color(0, 50, 0));
+      pixels.show();
+      Neo_p = Neo[i];
 
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+      break;
+    }
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL); // Pause before next pass through loop
+    if(i == 15){
+      pixels.setPixelColor(Neo[i], pixels.Color(0, 50, 0));
+      pixels.show();
+      Neo_p = Neo[i];
+    }
   }
+  // delay(10);
+}
+
+
+void serialEvent8(){
+  int n;
+  int x,y;
+  word revBuf_word[6];
+  byte revBuf_byte[6];
+  //受信データ数が、一定時間同じであれば、受信完了としてデータ読み出しを開始処理を開始する。
+  //受信データあり ※6バイト以上になるまでまつ
+  if(Serial8.available()>= 6){
+    //---------------------------
+    //受信データをバッファに格納
+    //---------------------------
+    n = 0;
+    while(Serial8.available()>0 ){ //受信データがなくなるまで読み続ける
+      //6バイト目まではデータを格納、それ以上は不要なデータであるため捨てる。
+      if(n < 6){
+        revBuf_byte[n] = Serial8.read();   //revBuf_byte[n] = Serial2.read();
+      }
+      else{
+        Serial8.read(); //Serial2.read();  //読みだすのみで格納しない。
+      }
+      n++;
+    }
+    //---------------------------
+    //データの中身を確認
+    //---------------------------
+    //データの先頭、終了コードあることを確認
+    if((revBuf_byte[0] == 0xFF ) && ( revBuf_byte[5] == 0xAA )){
+    //いったんWORD型（16bitデータ）としてから、int16_tとする。
+      revBuf_word[0] = (uint16_t(revBuf_byte[1])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
+      revBuf_word[1] = uint16_t(revBuf_byte[2]);//下位8ビットをbyteから、Wordに型変換して格納      
+      x = int16_t(revBuf_word[0]|revBuf_word[1]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
+      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
+      revBuf_word[2] = (uint16_t(revBuf_byte[3])<< 8);//上位8ビットをbyteから、Wordに型変換して格納　上位桁にするため8ビットシフト
+      revBuf_word[3] = uint16_t(revBuf_byte[4]);//下位8ビットをbyteから、Wordに型変換して格納      
+      y = int16_t(revBuf_word[2]|revBuf_word[3]);//上位8ビット、下位ビットを合成（ビットのORを取ることで格納する。）
+      // ※int ではなく　int16_t　にすることが必要。intだけだと、32ビットのintと解釈されマイナス値がマイナスとみなされなくなる、int16_tは、16ビット指定の整数型変換
+      
+      x = ball.ball_x.demandAve(x);
+      y = ball.ball_y.demandAve(y);
+    }
+    else{
+      // printf("ERR_REV");
+    }
+  }
+  // Serial.println("sawa");
+  // int a = Serial8.read();
+  // Serial.println(a);
 }
